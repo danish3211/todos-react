0920f9d1344428d6db8c5b129bde7ca2eb30e588
danish sheikh <130551259+danish3211@users.noreply.github.com>
2023-09-20 18:27:47 -0400

    add

--- src/components/TodoWraper.jsx
+++ src/components/TodoWraper.jsx
@@ -10,25 +10,19 @@ const TodoWraper = () => {
 
   const addTodo = todo => {
     setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }])
-    console.log(todos)
   }
-
   const toggleComplate = id => {
     setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
   }
-
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
-
-
   const editTodo = id => {
     setTodos(todos.map(todo => todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo))
   }
   const editTask = (task, id) => {
     setTodos(todos.map(todo => todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo))
   }
-
 
   return (
     <div className='TodoWrapper'>
@@ -46,7 +40,8 @@ const TodoWraper = () => {
         )
       ))}
     </div>
-  )
-}
+  )}
 
 export default TodoWraper
+
+
